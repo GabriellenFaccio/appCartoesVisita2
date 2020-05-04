c41c3a45ff4d5553e0f41f8252c1b01fe25102fe
Gabriellen Oliveira <gabriellen.oliveira@mbg.local>
2020-05-04 16:39:56 -0300

    projeto de cadastro de contatos, colocado o formulario a partir do botao

---
+++ application/views/formulario.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Formulario</title>
+</head>
+<body>
+	<form action="Welcome/cadastrarNovoContato" method="POST">
+		<label>Nome : </label>
+		<input type="text" name="nome">
+
+		<label>Contato : </label>
+		<input type="text" name="nome">
+
+		<input type="submit" name="salvar">
+	</form>
+</body>
+</html>
