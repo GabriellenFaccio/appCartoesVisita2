850c5b7ba25dca5f113dae1e0c8065e07c8881d4
Gabriellen Oliveira <gabriellen.oliveira@mbg.local>
2020-05-05 08:24:34 -0300

    implementado o recebimento de dados pelo método do botão salvar

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -2,19 +2,32 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
+	public $nome;
+	public $telefone;
+	public $email;
+
+
 	public function index(){
 		$this->load->view('welcome_message');
 	}
 
 
 	public function cadastrarNovoContato(){
-		echo "Cadastrar Novo Contato";
+		echo "Cadastrar Novo Contato <br>";
 		$this->load->view('formulario');
 	}
 
 
 	public function novoContato(){
 		echo "Novo contato";
+
+		$this->nome = $this->input->post('nome');
+		$this->email = $this->input->post('email');
+		$this->telefone = $this->input->post('telefone');
+
+		echo "<br>Nome : ".$this->nome."<br>";
+		echo "Email : ".$this->email."<br>";
+		echo "Telefone : ".$this->telefone."<br>";
 	}
 
 	public function buscarContato(){
--- application/views/formulario.php
+++ application/views/formulario.php
@@ -4,14 +4,17 @@
 	<title>Formulario</title>
 </head>
 <body>
-	<form action="novoContato" method="POST">
+	<form action="novoContato" method="POST"><br><br>
 		<label>Nome : </label>
-		<input type="text" name="nome">
+		<input type="text" name="nome" id="nome"><br><br>
 
-		<label>Contato : </label>
-		<input type="text" name="nome">
+		<label>Telefone : </label>
+		<input type="text" name="telefone" id="telefone"><br><br>
 
-		<input type="submit" name="salvar">
+		<label>Email : </label>
+		<input type="text" name="email" id="email"><br><br>
+
+		<input type="submit" name="salvar" value="Salvar">
 	</form>
 </body>
 </html>
